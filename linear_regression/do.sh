22a058fbd03a0217044ef08c4ae9cd91902210dc
Vincent Schramer <vinciple@gmail.com>
2014-02-24 16:59:18 -0600

    Tidied up graph generation script

--- linear_regression/do.sh
+++ linear_regression/do.sh
@@ -1,4 +1,22 @@
-for f in linreg_data/*.dat; do
-	gnuplot -e "filename='$f'" linreg.gp
+#!/bin/bash
 
+graphs_dir="graphs"
+fitlog="fit.log"
+
+mkdir $graphs_dir
+
+# For each datafile
+for f in linreg_data/*.dat; do
+
+    basename=`basename $f |cut -d. -f 1`
+    gp_output=$f.png
+    
+    # Generate graph
+    gnuplot -e "filename='$f'" linreg.gp
+    
+    # Move output file to graphs folder.
+    mv $gp_output $graphs_dir/$basename.png
+    
+    # Move best-fit line log
+    mv $fitlog $graphs_dir/$basename.fit.log
 done
